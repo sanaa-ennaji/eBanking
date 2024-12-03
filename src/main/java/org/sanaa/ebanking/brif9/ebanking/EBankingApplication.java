2ea3f2cb0fb031e33980920d0946fa641c1385fe
sanaa-ennaji <sanaaennaji93@gmail.com>
2024-12-03 17:27:21 +0100

    init commit

---
+++ src/main/java/org/sanaa/ebanking/brif9/ebanking/EBankingApplication.java
@@ -0,0 +1,14 @@
+package org.sanaa.ebanking.brif9.ebanking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EBankingApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(EBankingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/sanaa/ebanking/brif9/ebanking/EBankingApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sanaa.ebanking.brif9.ebanking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EBankingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
