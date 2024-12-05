8f4d70cc7327b4ead3fe4eab4fb9823ce406325a
sanaa-ennaji <sanaaennaji93@gmail.com>
2024-12-05 15:30:16 +0100

    project structure

---
+++ src/main/java/org/sanaa/ebanking/brif9/ebanking/controller/UserController.java
@@ -0,0 +1,4 @@
+package org.sanaa.ebanking.brif9.ebanking.controller;
+
+public class UserController {
+}
---
+++ src/main/java/org/sanaa/ebanking/brif9/ebanking/repository/UserRepository.java
@@ -0,0 +1,4 @@
+package org.sanaa.ebanking.brif9.ebanking.repository;
+
+public interface UserRepository {
+}
