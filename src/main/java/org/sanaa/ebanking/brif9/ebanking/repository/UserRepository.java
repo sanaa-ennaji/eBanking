b18eadf7aa1b16968135e0288ed34dd23f904d17
sanaa-ennaji <sanaaennaji93@gmail.com>
2024-12-06 17:01:40 +0100

    update: add  find by name method

--- src/main/java/org/sanaa/ebanking/brif9/ebanking/repository/UserRepository.java
+++ src/main/java/org/sanaa/ebanking/brif9/ebanking/repository/UserRepository.java
@@ -3,6 +3,9 @@ package org.sanaa.ebanking.brif9.ebanking.repository;
 import org.sanaa.ebanking.brif9.ebanking.models.entity.EbankUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<EbankUser , Long>  {
+import java.util.Optional;
 
+public interface UserRepository extends JpaRepository<EbankUser , Long>  {
+    Optional<EbankUser> findByUsername(String username);
 }
+
