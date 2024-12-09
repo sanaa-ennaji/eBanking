abe3c941cf66db9b4009c45cfb4f89961e998eb2
sanaa-ennaji <sanaaennaji93@gmail.com>
2024-12-09 17:15:42 +0100

    add encrypting password code

--- src/main/java/org/sanaa/ebanking/brif9/ebanking/service/UserService.java
+++ src/main/java/org/sanaa/ebanking/brif9/ebanking/service/UserService.java
@@ -5,6 +5,7 @@ import org.sanaa.ebanking.brif9.ebanking.models.dto.Response.UserResponseDTO;
 import org.sanaa.ebanking.brif9.ebanking.models.entity.EbankUser;
 import org.sanaa.ebanking.brif9.ebanking.models.mapper.UserMapper;
 import org.sanaa.ebanking.brif9.ebanking.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,13 +13,17 @@ public class UserService implements UserServiceI {
 
 private final UserRepository userRepository;
 private final UserMapper userMapper;
+private final PasswordEncoder passwordEncoder;
 
-    public UserService(UserRepository userRepository, UserMapper userMapper) {
+    public UserService(UserRepository userRepository, UserMapper userMapper, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
         this.userMapper = userMapper;
+        this.passwordEncoder = passwordEncoder;
     }
     @Override
     public UserResponseDTO register (UserRequestDTO requestDTO){
+        String encodedPassword = passwordEncoder.encode(requestDTO.getPassword());
+        requestDTO.setPassword(encodedPassword);
         EbankUser user = userMapper.toEntity(requestDTO);
         EbankUser savedUser = userRepository.save(user);
         return userMapper.toResponseDTO(savedUser);
